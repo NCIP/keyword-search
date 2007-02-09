752b161eea827705806ce992e57ed83e953cd23f
juberahamad_patel <juberahamad_patel>
2007-02-09 09:11:04 +0000

    a brief structure for TiTLi
    SVN-Revision: 2

---
+++ trunk/titli/model/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package titli.model;
+
+/**
+ * @author juberahamad_patel
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
