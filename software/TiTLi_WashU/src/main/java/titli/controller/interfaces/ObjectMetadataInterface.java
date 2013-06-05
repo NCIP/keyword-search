8d1285ebd9fcfc26a67c5d68f0c265eb9f638d91
Mahidhar Narra <narram@mail.nih.gov>
2013-06-05 13:48:54 -0400

    updated license headers

--- software/TiTLi_WashU/src/main/java/titli/controller/interfaces/ObjectMetadataInterface.java
+++ software/TiTLi_WashU/src/main/java/titli/controller/interfaces/ObjectMetadataInterface.java
@@ -1,3 +1,10 @@
+/*L
+ * Copyright Washington University in St. Louis, SemanticBits, Persistent Systems, Krishagni.
+ *
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/catissue-keyword-search/LICENSE.txt for details.
+ */
+
 package titli.controller.interfaces;
 
 
